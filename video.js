9b88d7f464b693cb2c67ac870c64b60b4d7fa877
Vale-com <valegarco18@gmail.com>
2022-05-28 10:03:08 -0500

    ohh party party yeah

--- video.js
+++ video.js
@@ -17,16 +17,53 @@ botonPause.addEventListener("click" ,()=>{
     sunset.pause();
 })
 
-var botonReiniciar= document.getElementById("botonReiniciar")
+var botonReiniciar= document.getElementById("botonReiniciar");
 
 botonReiniciar.addEventListener("click" ,()=>{
     sunset.currentTime= 0
 })
 
 sunset.addEventListener("click" ,()=>{
+    if(sunset.paused==true) {
     sunset.play();
+    }
+    else if (sunset.paused==false) {
+    sunset.pause();
+    }         
 })
 
-sunset.addEventListener("dblclick" ,()=>{
-    sunset.pause();
+var audioPrueba= document.getElementById("audioPrueba");
+
+var btnPlayAudio= document.getElementById("btnPlayAudio");
+
+btnPlayAudio.addEventListener("click" ,()=>{
+    if(audioPrueba.paused == true) {
+        audioPrueba.play();
+    }
+    else if(audioPrueba.paused == false) {
+        audioPrueba.pause();
+    }
+})
+
+var before= document.getElementById("before");
+var after= document.getElementById("after");
+
+before.addEventListener("click" ,()=>{
+    before.currentTime= 5;
+})
+
+after.addEventListener("click" ,()=>{
+    after.currentTime= 15;
+})
+
+var volumenm= document.getElementById("volumenm");
+
+var volumena= document.getElementById("volumena");
+
+volumenm.addEventListener("click" ,()=>{
+    volumenm.volume = 0.1;
+})
+
+volumena.addEventListener("click" ,()=>{
+    volumena.volume = 0.5;
 })
