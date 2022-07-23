0a2555a886b43056a9e6ee1237fe899951e61556
Vale-com <valegarco18@gmail.com>
2022-07-23 10:00:59 -0500

    triqui

--- Triki/triki.js
+++ Triki/triki.js
@@ -1,4 +1,4 @@
-var turno = "boton"
+var turno = "estrella"
 
 var TodasLasCasillas=document.getElementsByClassName("casilla")
 TodasLasCasillas= Array.from(TodasLasCasillas);
@@ -8,13 +8,31 @@ for (const index in TodasLasCasillas){
     let Elemento = TodasLasCasillas[index];
     Elemento.addEventListener("click",()=>{
         let img = Elemento.querySelector("img");
-        if (turno == "cuadrado") {
-            img.src = "cuadrado.svg";
+        if (turno == "estrella") {
+            img.src = "estrella.svg";
             turno = "circulo"
         } else{
             img.src = "circulo.svg";
-            turno = "cuadrado";
+            turno = "estrella";
         }
     })
 }
 
+for (const index in TodasLasCasillas){
+    console.log("Creando evento para la casilla: "+index);
+    let Elemento = TodasLasCasillas[index];
+    Elemento.addEventListener("dblclick",()=>{
+        let img = Elemento.querySelector("img");
+        img.src= "";
+    })
+}
+
+var botonReiniciar= document.getElementById("botonReiniciar");
+
+botonReiniciar.addEventListener("click" ,()=>{
+    for (const index in TodasLasCasillas){
+        let img =TodasLasCasillas[index].querySelector("img");
+        img.src = "";
+    }
+    turno= "estrella"
+})
